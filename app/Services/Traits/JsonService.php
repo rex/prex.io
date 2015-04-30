a71fab69d4e6826d68e23ba5e92d89a854c34e71
Pierce Moore <me@prex.io>
2015-04-29 21:31:27 -0500

    Added transform method to JsonService trait

--- app/Services/Traits/JsonService.php
+++ app/Services/Traits/JsonService.php
@@ -6,12 +6,20 @@ trait JsonService {
    * @param $endpoint String The URI for the desired endpoint
    * @param $response_key String If a nested response, return this key from the response array
    */
-  public static function fetchJson($endpoint, $response_key = null) {
+  public static function fetchJson($endpoint, $response_key = null, $transform_result = null) {
     $response = self::client()->get($endpoint);
 
+    if($transform_result == null)
+      $transform_result = function($val) { return $val; };
+
     if($response_key)
-      return $response->json()[$response_key];
+      $ret = $response->json()[$response_key];
     else
-      return $response->json();
+      $ret = $response->json();
+
+    if(is_array($ret))
+      return array_map($transform_result, $ret);
+    else
+      return $transform_result($ret);
   }
 }
