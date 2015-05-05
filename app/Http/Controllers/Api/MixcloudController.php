7235e3de6ebb48591c5a1c438d9c45ea89bd0cf6
Pierce Moore <me@prex.io>
2015-05-05 11:04:48 -0500

    Updated Mixcloud to new service structure

--- app/Http/Controllers/Api/MixcloudController.php
+++ app/Http/Controllers/Api/MixcloudController.php
@@ -5,6 +5,10 @@ use App\Services\Mixcloud;
 class MixcloudController extends ApiController {
   public $route_namespace = "\Api\Mixcloud";
 
+  public function __construct() {
+    $this->mixcloud = new Mixcloud();
+  }
+
   public function index() {
     return $this->endpointRoot();
   }
@@ -14,23 +18,14 @@ class MixcloudController extends ApiController {
   }
 
   public function user($username) {
-    if($username == "self")
-      $username = env('MIXCLOUD_HANDLE');
-
-    return $this->respond( Mixcloud::user($username) );
+    return $this->respond( $this->mixcloud->user($username) );
   }
 
   public function cloudcasts($username) {
-    if($username == "self")
-      $username = env('MIXCLOUD_HANDLE');
-
-    return $this->respond( Mixcloud::cloudcasts($username) );
+    return $this->respond( $this->mixcloud->cloudcasts($username) );
   }
 
   public function cloudcast($username, $cloudcast_slug) {
-    if($username == "self")
-      $username = env('MIXCLOUD_HANDLE');
-
-    return $this->respond( Mixcloud::cloudcast($username, $cloudcast_slug) );
+    return $this->respond( $this->mixcloud->cloudcast($username, $cloudcast_slug) );
   }
 }
--- app/Services/Mixcloud.php
+++ app/Services/Mixcloud.php
@@ -4,26 +4,48 @@ use GuzzleHttp\Client as Http;
 use App\Services\Traits\JsonService;
 use App\Models\Mixcloud\User;
 use App\Models\Mixcloud\Cloudcast;
+use Config;
 
-class Mixcloud {
-  use JsonService;
-
-  public static function users() {
+class Mixcloud extends BaseService {
+  public function __construct() {
+    parent::__construct();
+    $this->cache_namespace = Config::get('services.mixcloud.cache_namespace');
+    $this->cache_ttl = Config::get('services.mixcloud.cache_ttl');
+    $this->http_client_options = [
+      'base_url' => "http://api.mixcloud.com/"
+    ];
   }
 
-  public static function user($username) {
-    return self::fetchJson($username);
+  public function users() {
+    //
   }
 
-  public static function cloudcasts($username) {
-    return self::fetchJson("$username/cloudcasts", "data");
+  public function user($username) {
+    $username = $this->getUsername($username);
+
+    return $this->fetchAndCache($username, [
+      'cache_key' => "users:$username"
+    ]);
   }
 
-  public static function cloudcast($username, $cloudcast_slug) {
-    return self::fetchJson("$username/$cloudcast_slug");
+  public function cloudcasts($username) {
+    $username = $this->getUsername($username);
+
+    return $this->fetchAndCache("$username/cloudcasts", [
+      "response_key" => "data"
+    ]);
   }
 
-  private static function client() {
-    return new Http(['base_url' => "http://api.mixcloud.com/"]);
+  public function cloudcast($username, $cloudcast_slug) {
+    return $this->fetchAndCache("$username/$cloudcast_slug", [
+      'cache_key' => "$username/cloudcasts/$cloudcast_slug"
+    ]);
+  }
+
+  private function getUsername($username) {
+    if($username == null || $username == "self")
+      return Config::get('services.mixcloud.handle');
+    else
+      return $username;
   }
 }
