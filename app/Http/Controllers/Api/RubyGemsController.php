c0e59eef84014124e040fd648eeb89edae943ba9
Pierce Moore <me@prex.io>
2015-05-05 13:33:13 -0500

    Added RubyGems to new service structure

--- app/Http/Controllers/Api/RubyGemsController.php
+++ app/Http/Controllers/Api/RubyGemsController.php
@@ -5,15 +5,19 @@ use App\Services\RubyGems;
 class RubyGemsController extends ApiController {
   public $route_namespace = "\Api\RubyGems";
 
+  public function __construct() {
+    $this->rubygems = new RubyGems();
+  }
+
   public function index() {
     return $this->endpointRoot();
   }
 
   public function gems() {
-    return $this->respond( RubyGems::gems() );
+    return $this->respond( $this->rubygems->gems() );
   }
 
   public function gem($gem_name) {
-    return $this->respond( RubyGems::gem($gem_name) );
+    return $this->respond( $this->rubygems->gem($gem_name) );
   }
 }
--- app/Services/RubyGems.php
+++ app/Services/RubyGems.php
@@ -2,26 +2,32 @@
 
 use GuzzleHttp\Client as Http;
 use App\Services\Traits\JsonService;
+use Config;
 
-class RubyGems {
-  use JsonService;
-
-  public static function gems() {
-    return self::fetchJson("gems.json");
-  }
-
-  public static function gem($gem_name) {
-    return self::fetchJson("gems/$gem_name.json");
-  }
-
-  private static function client() {
-    return new Http([
+class RubyGems extends BaseService {
+  public function __construct() {
+    parent::__construct();
+    $this->cache_namespace = Config::get('services.rubygems.cache_namespace');
+    $this->cache_ttl = Config::get('services.rubygems.cache_ttl');
+    $this->http_client_options = [
       'base_url' => 'https://rubygems.org/api/v1/',
       'defaults' => [
         'headers' => [
           'Authorization' => env('RUBYGEMS_ACCESS_TOKEN')
         ]
       ]
+    ];
+  }
+
+  public function gems() {
+    return self::fetchAndCache("gems.json", [
+      'cache_key' => 'gems'
+    ]);
+  }
+
+  public function gem($gem_name) {
+    return self::fetchAndCache("gems/$gem_name.json", [
+      'cache_key' => 'gems'
     ]);
   }
 }
